dcdb3a408a40861922ead4f2d271847d95551df3
Saul Johnson <saul.a.johnson@gmail.com>
2018-04-17 15:40:38 +0100

    Add symbol mapper

---
+++ Capitalizer.SymbolMapper/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Capitalizer.Shared;
+using System.Linq;
+
+namespace Capitalizer.SymbolMapper
+{
+    class Program
+    {
+        /// <summary>
+        /// Returns true if a character is neither a letter nor a digit.
+        /// </summary>
+        /// <param name="chr">The character to check.</param>
+        /// <returns></returns>
+        private static bool IsOther(char chr)
+        {
+            return !(char.IsDigit(chr) || char.IsLower(chr) || char.IsUpper(chr));
+        }
+
+        static void Main(string[] args)
+        {
+            // Read in entire file.
+            var lines = FileUtils.ReadFileAsLines(args[0]);
+
+            // Should we go from the end instead?
+            var fromEnd = args.Length > 1 && args[1] == "-r";
+
+            // Build a symbol 'heatmap' by index of occurence.
+            var dict = new SortedDictionary<int, int>();
+            foreach (var line in lines)
+            {
+                for (int j = 0; j < line.Length; j++)
+                {
+                    var k = fromEnd ? line.Length - 1 - j : j;
+                    if (IsOther(line[k]))
+                    {
+                        // Increment entry in symbol heatmap.
+                        if (!dict.ContainsKey(j))
+                        {
+                            dict.Add(j, 0);
+                        }
+                        dict[j]++;
+                    }
+                }
+            }
+
+            // Print out total.
+            var totalCount = lines.Count(x => x.Any(y => IsOther(y)));
+            Console.WriteLine($"A total of {totalCount}/{lines.Count()} contain symbol" +
+                $" characters ({(totalCount/lines.Count())*100}%).");
+
+            // Add keys (indices) to list.
+            var indices = new List<int>();
+            foreach (var entry in dict)
+            {
+                indices.Add(entry.Key);
+            }
+
+            // Sort indices.
+            var keys = indices.ToArray();
+            Array.Sort(keys);
+
+            // Print out indices in order with corresponding number of occurences.
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine($"{keys[i]}:{dict[keys[i]]}");
+            }
+        }
+    }
+}
